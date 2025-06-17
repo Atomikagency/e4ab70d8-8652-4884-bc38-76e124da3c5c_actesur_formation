3acbb981e4937081626d521e1a635db3f31e1a74
Clement Menczynski <71343161+clementmenc@users.noreply.github.com>
2025-06-17 17:22:41 +0200

    Add formation_list shortcode style

--- src/includes/formation_list.shortcode.php
+++ src/includes/formation_list.shortcode.php
@@ -24,34 +24,38 @@ add_shortcode('formation_list', function ($atts) {
     }
 
     $formations = get_posts($args);
+    $categories = get_formation_categories();
 
     if (empty($formations)) {
-        return '<p>Aucune formation trouvée.</p>';
+        return '<p>Aucune formation trouvÃ©e.</p>';
     }
+    // print("<pre>".print_r($formations,true)."</pre>");
 
-    $output = '<div class="formation-list">';
+    $formations_by_categorie = [];
     
     foreach ($formations as $formation) {
-        $categorie = get_post_meta($formation->ID, '_formation_categorie', true);
-        $description_courte = get_post_meta($formation->ID, '_formation_description_courte', true);
-        $categories = get_formation_categories();
-        $categorie_label = isset($categories[$categorie]) ? $categories[$categorie] : $categorie;
-        
-        $output .= '<div class="formation-item">';
-        $output .= '<h3><a href="' . get_permalink($formation->ID) . '">' . esc_html($formation->post_title) . '</a></h3>';
-        
-        if ($categorie) {
-            $image_url = ACTESUR_FORMATION_PLUGIN_URL . 'images/' . $categorie . '.svg';
-            $output .= '<div class="formation-categorie">';
-            $output .= '<img src="' . esc_url($image_url) . '" alt="' . esc_attr($categorie_label) . '" style="width:30px;height:auto;"> ';
-            $output .= '<span>' . esc_html($categorie_label) . '</span>';
+        $cat_slug = get_post_meta($formation->ID, '_formation_categorie', true);
+        if (!isset($formations_by_categorie[$cat_slug])) {
+            $formations_by_categorie[$cat_slug] = [];
+        }
+        $formations_by_categorie[$cat_slug][] = $formation;
+    }
+
+    $output = '<div class="actesur-categorie-list">';
+    foreach($formations_by_categorie as $slug => $categorie){
+        $url = site_url('/formations/?categorie=' . $slug);
+        $image_url = ACTESUR_FORMATION_PLUGIN_URL.'images/'.$slug . '.svg';
+        $output .= '<div class="categorie-item">';
+        $output .= '<h3><img src="' . esc_url($image_url) . '"/><a href="' . esc_url($url) . '">' . esc_html($categories[$slug]) . '</a></h3>';
+
+        foreach ($categorie as $formation) {
+            $formation_url = get_permalink($formation);
+            $formation_title = get_the_title($formation);
+            $output .= '<div class="formation-item">';
+            $output .= '<a href="' . esc_url($formation_url) . '">' . esc_html($formation_title) . '<img src="' . esc_url(ACTESUR_FORMATION_PLUGIN_URL.'images/arrow-right.svg') .'" /></a>';
             $output .= '</div>';
         }
-        
-        if ($description_courte) {
-            $output .= '<div class="formation-description">' . nl2br(esc_html($description_courte)) . '</div>';
-        }
-        
+
         $output .= '</div>';
     }
     
